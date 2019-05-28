04622e9ecf8d86d9333b3c0952e98766f05baece
pipecato2 <51090587+pipecato2@users.noreply.github.com>
2019-05-27 22:11:40 -0400

    tiene largo 8 la contraseña

--- app/src/main/java/com/example/max/sysmarket2/vista/LoginActivity.java
+++ app/src/main/java/com/example/max/sysmarket2/vista/LoginActivity.java
@@ -1,12 +1,24 @@
 package com.example.max.sysmarket2.vista;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import com.example.max.sysmarket2.R;
 import com.example.max.sysmarket2.modelo.Validadores;
@@ -37,7 +49,6 @@ public class LoginActivity extends AppCompatActivity {
     Button btn_lg_salir_A;
 
     String titulo = "Ingreso";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,16 +83,18 @@ public class LoginActivity extends AppCompatActivity {
                  */
 
 
-                if (Validadores.valida_existencia(usu) == null){
+           //     if (Validadores.valida_existencia(usu) == null){
 
-                    Intent i = new Intent(LoginActivity.this, ComprasActivity.class);
-                    startActivity(i);
+             //       Intent i = new Intent(LoginActivity.this, ComprasActivity.class);
+               //     startActivity(i);
 
-                }else {
+               // }else {
 
-                    error = Validadores.valida_existencia(usu);
-                    Toast.makeText(getApplicationContext(), error,Toast.LENGTH_SHORT).show();
-                }
+                 //   error = Validadores.valida_existencia(usu);
+                 //   Toast.makeText(getApplicationContext(), error,Toast.LENGTH_SHORT).show();
+               // }
+                    getSede(v.getContext(),et_lg_usuario_A.getText().toString(),et_lg_password_A.getText().toString());
+
 
 
          /*
@@ -116,7 +129,55 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    ;
+    public static void getSede(Context contex, String usuario, String Clave){
+
+        String PLACES_URL = "http://fmorales-001-site2.btempurl.com/api/Usuario/Admin/12345678/ValidacionUsuario";
+        final String LOG_TAG = "VolleyPlacesRemoteDS";
+
+// Instantiate the RequestQueue
+        RequestQueue requestQueue = Volley.newRequestQueue(contex);
+
+//Prepare the Request
+        JsonArrayRequest request = new JsonArrayRequest(
+                Request.Method.GET, //GET or POST
+                PLACES_URL, //URL
+                null, //Parameters
+                new Response.Listener<JSONArray>() { //Listener OK
+
+                    @Override
+                    public void onResponse(JSONArray responsePlaces) {
+                        for (int i=0; i<responsePlaces.length();i++){
+                            try {
+                                //lstSede.add(new Sede(responsePlaces.getJSONObject(i).getInt("sede_id"),
+                                //        responsePlaces.getJSONObject(i).getString("sede_direccion"),
+                                //       responsePlaces.getJSONObject(i).getInt("sede_estado")));
+                                Log.e("tag_usuario",responsePlaces.getJSONObject(i).getString("usuario1"));
+                                Log.e("tag_rut",responsePlaces.getJSONObject(i).getString("Rut1"));
+                                Log.e("tag_nombre",responsePlaces.getJSONObject(i).getString("Nombre1"));
+                                Log.e("tag_IdPerfil",responsePlaces.getJSONObject(i).getString("IdPerfil"));
+                                Log.e("tag_Fecha_Nac1",responsePlaces.getJSONObject(i).getString("Fecha_Nac1"));
+                                Log.e("tag_Email1",responsePlaces.getJSONObject(i).getString("Email1"));
+                                Log.e("tag_Clave1",responsePlaces.getJSONObject(i).getString("Clave1"));
+                                Log.e("tag_Apellido1",responsePlaces.getJSONObject(i).getString("Apellido1"));
+                                Log.e("tag_Activo",responsePlaces.getJSONObject(i).getString("Activo"));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        Log.d(LOG_TAG,responsePlaces.toString());
+                    }
+                }, new Response.ErrorListener() { //Listener ERROR
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //There was an error :(
+                Log.d(LOG_TAG,error.toString());
+            }
+        });
+
+//Send the request to the requestQueue
+        requestQueue.add(request);
+    }
 
 
 }
