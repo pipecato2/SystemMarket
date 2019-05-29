e2e8c732a7b6d466752a8d02dd0d6e136061ae9a
pipecato2 <51090587+pipecato2@users.noreply.github.com>
2019-05-28 21:14:40 -0400

    tiene largo 8 la contraseña

---
+++ app/src/main/java/com/example/max/sysmarket2/vista/RecuperarActivity.java
@@ -0,0 +1,15 @@
+package com.example.max.sysmarket2.vista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.max.sysmarket2.R;
+
+public class RecuperarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar);
+    }
+}
