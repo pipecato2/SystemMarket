9f837fb7a9f9c470c4aed0670d512fc94e00d9d8
Maximiliano Pinto <maximilianoalberto.pinto@gmail.com>
2019-06-21 15:55:58 -0400

    Se corrige metodo para camara. El problema del escano se debia a que no se puede ingresar la funcionalidad dentro del onClick de img_btn_scan ya que scanear() inicia una nueva actividad, la funcionalidad se esta desarrollando dentro de onActivityResult ya que alli es donde llega el resultado del escaneo

--- app/src/main/java/com/example/max/sysmarket2/vista/ComprasActivity.java
+++ app/src/main/java/com/example/max/sysmarket2/vista/ComprasActivity.java
@@ -1,6 +1,7 @@
 package com.example.max.sysmarket2.vista;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -9,6 +10,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.max.sysmarket2.R;
+import com.example.max.sysmarket2.modelo.conexion.ConexionSQLiteHelper;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -18,6 +20,8 @@ public class ComprasActivity extends AppCompatActivity {
     EditText et_cc_producto;
     ImageButton img_btn_scan;
     String res;
+    String resScan;
+    ConexionSQLiteHelper conn;
 
 
 
@@ -32,9 +36,15 @@ public class ComprasActivity extends AppCompatActivity {
         scanear();
     }
 
-    public void scanear (){
+    /**
+     *  Prepara la camara del telefono movil para iniciar el escaneo.
+     *
+     */
+
+    public void scanear() {
+
         IntentIntegrator intent = new IntentIntegrator(this);
-        intent.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+        intent.setDesiredBarcodeFormats(IntentIntegrator.PRODUCT_CODE_TYPES);
         intent.setPrompt("Escanear codigo");
         intent.setCameraId(0);
         intent.setOrientationLocked(false);
@@ -43,19 +53,37 @@ public class ComprasActivity extends AppCompatActivity {
         intent.initiateScan();
 
     }
+
+    /**
+     * El metodo inicia la nueva actividad en donde reciibe el codigo de barras, busca el formato y devuelve
+     * como un String, se valida existencia de codigo de barra, aun esta en desarrollo el resto de la funcionalidad
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         super.onActivityResult(requestCode, resultCode, data);
 
         final IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-
+        String codigo_de_barra;
         if (result != null) {
+            resScan = result.getContents();
+            conn = new ConexionSQLiteHelper(ComprasActivity.this, "systemMarket", null, 1);
+            Cursor cursor = conn.alldataProd();
+            while (cursor.moveToNext()) {
+                codigo_de_barra = String.valueOf(cursor.getInt(0));
+                if (codigo_de_barra.equals(resScan)) {
+                    Toast.makeText(this, "Existe" + codigo_de_barra, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "No existe" + codigo_de_barra, Toast.LENGTH_SHORT).show();
 
-            res = result.getContents();
-            Toast.makeText(this, res, Toast.LENGTH_SHORT).show();
-            et_cc_producto.setText(res);
-        } else {
-            Toast.makeText(this, "No hay", Toast.LENGTH_SHORT).show();
+                }
+            }
         }
+
+
     }
 
 
